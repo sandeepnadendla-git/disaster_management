660a9cb638c834fc016e97ca5180b375fe0e1583
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2021-09-29 09:20:00 -0500

    Changes approved login

--- src/LogIn.js
+++ src/LogIn.js
@@ -5,25 +5,23 @@ class LogIn extends Component {
   render() {
     return (
 
-      <div className="Login">
-
-
-        <div class="login-wrap">
-          <div class="login-html">
-            <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab">Sign In</label>
-            <input id="tab-2" type="radio" name="tab" class="sign-up" /><label for="tab-2" class="tab">Sign Up</label>
-            <div class="login-form">
+      <div class="login-wrap">
+        <div class="login-html">
+          <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab" >Sign In</label>
+          <input id="tab-2" type="radio" name="tab" class="sign-up" /><label for="tab-2" class="tab">Sign Up</label>
+          <div class="login-form">
+            <form>
               <div class="sign-in-htm">
                 <div class="group">
                   <label for="user" class="label">Username</label>
-                  <input id="user" type="text" class="input" />
+                  <input id="user" type="text" class="input" required />
                 </div>
                 <div class="group">
                   <label for="pass" class="label">Password</label>
-                  <input id="pass" type="password" class="input" data-type="password" />
+                  <input id="pass" type="password" class="input" data-type="password" required />
                 </div>
                 <div class="group">
-                  <input id="check" type="checkbox" class="check" checked />
+                  <input id="check" type="checkbox" class="check" />
                   <label for="check"><span class="icon"></span> Keep me Signed in</label>
                 </div>
                 <div class="group">
@@ -34,22 +32,24 @@ class LogIn extends Component {
                   <a href="#forgot">Forgot Password?</a>
                 </div>
               </div>
+            </form>
+            <form>
               <div class="sign-up-htm">
                 <div class="group">
                   <label for="user" class="label">Username</label>
-                  <input id="user" type="text" class="input" />
+                  <input id="user" type="text" class="input" required />
                 </div>
                 <div class="group">
                   <label for="pass" class="label">Password</label>
-                  <input id="pass" type="password" class="input" data-type="password" />
+                  <input id="pass" type="password" class="input" data-type="password" required />
                 </div>
                 <div class="group">
                   <label for="pass" class="label">Repeat Password</label>
-                  <input id="pass" type="password" class="input" data-type="password" />
+                  <input id="pass" type="password" class="input" data-type="password" required />
                 </div>
                 <div class="group">
                   <label for="pass" class="label">Email Address</label>
-                  <input id="pass" type="text" class="input" />
+                  <input id="pass" type="text" class="input" required />
                 </div>
                 <div class="group">
                   <input type="submit" class="button" value="Sign Up" />
@@ -59,12 +59,11 @@ class LogIn extends Component {
                   <label for="tab-1">Already Member?</label>
                 </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
-
-
       </div>
+
     )
   }
 }
