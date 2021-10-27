290889545b6ff964179d955d1e58669b0251ec8a
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2021-10-27 09:45:31 -0500

    reports

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Route, BrowserRouter,Switch } from 'react-router-dom';
 import LogIn from './components/LogIn'
 import Dashboard from './components/Dashboard'
+import report from './components/Report.js'
 
 export class App extends Component {
   render() {
@@ -14,6 +15,7 @@ export class App extends Component {
             <Route exact path='/' component={LogIn} />
             <Route  path='/login' component={LogIn} />
             <Route  path='/dashboard' component={Dashboard} />
+            <Route  path='/report' component={report} />
             
           </Switch>
         </div>
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,7 +6,7 @@ function Dashboard() {
         <div>
             <ul>
                 <li><a href="#home">Home</a></li>
-                <li><a href="#Report">Reports</a></li>
+                <li><a href="/Report">Reports</a></li>
                 <li><a href="#contact">Report Incident</a></li>
                 <li style={{float:'right'}}><a class="active" href="/login">Log Out</a></li>
             </ul>
