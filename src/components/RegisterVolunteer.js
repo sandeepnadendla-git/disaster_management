17cb23fb1d62636f9e388063422069ede6c1de0f
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2022-02-21 09:01:16 -0600

    added password

--- src/components/RegisterVolunteer.js
+++ src/components/RegisterVolunteer.js
@@ -20,28 +20,33 @@ const { useState } = React;
 
 function saveIncident(e) {
     e.preventDefault();
-    const usersInfo = {
-        firstName: document.getElementById('FirstName').value,
-        lastName: document.getElementById('LastName').value,
-        emailAddress: document.getElementById('EmailAddress').value,
-        contactNumber: document.getElementById('ContactNumber').value,
-        streetAddress: document.getElementById('Address').value,
-        city: document.getElementById('City').value,
-        state: document.getElementById('State').value,
-        zipCode: document.getElementById('Zipcode').value,
-        qualification: document.getElementById('Qualification').value,
-        createdAt: Timestamp.now().toDate().toString(),
-        role: 'Volunteer'
+    if (document.getElementById('savePass').value != document.getElementById('ChkPass').value) {
+        alert("Password and repeat password doesn't match");
+    } else {
+        const usersInfo = {
+            firstName: document.getElementById('FirstName').value,
+            lastName: document.getElementById('LastName').value,
+            emailAddress: document.getElementById('EmailAddress').value,
+            contactNumber: document.getElementById('ContactNumber').value,
+            streetAddress: document.getElementById('Address').value,
+            city: document.getElementById('City').value,
+            state: document.getElementById('State').value,
+            zipCode: document.getElementById('Zipcode').value,
+            password:document.getElementById('savePass').value,
+            repeatPassword:document.getElementById('ChkPass').value,
+            qualification: document.getElementById('Qualification').value,
+            createdAt: Timestamp.now().toDate().toString(),
+            role: 'Volunteer'
+        }
+
+
+        try {
+            const docRef = addDoc(collection(analytics, "usersDB"), usersInfo);
+            alert("Users added successfully");
+        } catch (e) {
+            alert("Error adding document: ", e);
+        }
     }
-
-
-    try {
-        const docRef = addDoc(collection(analytics, "usersDB"), usersInfo);
-        alert("Users added successfully");
-    } catch (e) {
-        alert("Error adding document: ", e);
-    }
-
 }
 
 function close() {
@@ -56,8 +61,8 @@ function RegisterVolunteer() {
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     const [state, setState] = useState(null);
-   
-    
+
+
     console.log(state);
     return (
         <div>
@@ -99,7 +104,7 @@ function RegisterVolunteer() {
                                     <input class="form-control col-form col-sm-6" type="text" id="Address" required />
                                 </div>
                                 <div class="col-sm-5">
-                                <legend class="col-form-label col-sm-4 pt-0">City:<span className="impFld">*</span> </legend>
+                                    <legend class="col-form-label col-sm-4 pt-0">City:<span className="impFld">*</span> </legend>
                                     <input class="form-control col-form col-sm-6" type="text" id="City" required />
                                 </div>
                             </div>
@@ -112,29 +117,41 @@ function RegisterVolunteer() {
                                     <input class="form-control col-form col-sm-6" type="text" id="State" required />
                                 </div>
                                 <div class="col-sm-5">
-                                <legend class="col-form-label col-sm-4 pt-0">Zipcode:<span className="impFld">*</span> </legend>
+                                    <legend class="col-form-label col-sm-4 pt-0">Zipcode:<span className="impFld">*</span> </legend>
                                     <input class="form-control col-form col-sm-6" type="number" id="Zipcode" required />
                                 </div>
                             </div>
                         </fieldset>
 
+                        <fieldset class="form-group mb-3">
+                            <div class="row">
+                                <div class="col-sm-5">
+                                    <legend class="col-form-label col-sm-4 pt-0">Password:<span className="impFld">*</span></legend>
+                                    <input class="form-control col-form col-sm-6" type="password" id="savePass" required />
+                                </div>
+                                <div class="col-sm-5">
+                                    <legend class="col-form-label col-sm-6 pt-0">Repeat Password:<span className="impFld">*</span> </legend>
+                                    <input class="form-control col-form col-sm-6" type="password" id="ChkPass" required />
+                                </div>
+                            </div>
+                        </fieldset>
 
                         <fieldset class="form-group mb-3">
                             <div class="row">
-                            
+
                                 <div class="col-sm-10">
                                     <legend class="col-form-label col-sm-4 pt-0">Qualification:<span className="impFld">*</span></legend>
                                     <input class="form-control" list="datalistOptions" id="Qualification" placeholder="Type to search..."></input>
                                     <datalist id="datalistOptions">
-                                    <option value="Medical Doctor"></option>
-                                    <option value="Nurse"></option>
-                                    <option value="Paramedic"></option>
-                                    <option value="EMTB"></option>
-                                    <option value="EMR"></option>
-                                    <option value="Fire Fighter"></option>
-                                    <option value="CERT Trained"></option>
-                                    <option value="Law Inforcement"></option>
-                                    <option value="Others"></option>
+                                        <option value="Medical Doctor"></option>
+                                        <option value="Nurse"></option>
+                                        <option value="Paramedic"></option>
+                                        <option value="EMTB"></option>
+                                        <option value="EMR"></option>
+                                        <option value="Fire Fighter"></option>
+                                        <option value="CERT Trained"></option>
+                                        <option value="Law Inforcement"></option>
+                                        <option value="Others"></option>
                                     </datalist>
                                 </div>
                             </div>
