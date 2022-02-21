5e56db32a839456343b8204bb86066333da3a35d
PariveshitaThota <77807971+PariveshitaThota@users.noreply.github.com>
2022-02-20 20:40:03 -0600

    modified UI

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,16 @@
 import React from 'react'
-
+import {NavLink} from 'react-router-dom'
 function Layout() {
     return (
 
         <div>
             <ul className='ulLayout'>
-                <li><a href="/Dashboard">Home</a></li>
-                <li><a href="/Report">Reports</a></li>
-                <li><a href="/incidentreport">Report Incident</a></li>
-                <li><a href="/RegisterVolunteer">Register Volunteer</a></li>
-                <li><a href="/users">Users</a></li>
+                
+                <NavLink exact to ="/Dashboard" className="main-Nav" activeClassName = "main-nav-active"><li><a>Home</a></li></NavLink>
+                <NavLink  exact to ="/Report" className="main-Nav" activeClassName = "main-nav-active"><li><a>Reports</a></li></NavLink>
+                <NavLink  exact to ="/incidentreport" className="main-Nav" activeClassName = "main-nav-active"><li><a>Report Incident</a></li></NavLink>
+                <NavLink  exact to ="/RegisterVolunteer" className="main-Nav" activeClassName = "main-nav-active"><li ><a>Register Volunteer</a></li></NavLink>
+                <NavLink  exact to ="/users" className="main-Nav" activeClassName = "main-nav-active"><li><a href="/users">Users</a></li></NavLink>
                 <li style={{ float: 'right' }}><a class="active" href="/login">Log Out</a></li>
             </ul>
         </div>
