71a75f65c5f5ce02e3cb99a57abc09bfcf88d842
sandeepnadendla-git <77738760+sandeepnadendla-git@users.noreply.github.com>
2022-04-08 16:13:35 -0500

    login dash to report change

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -44,7 +44,7 @@ class LogIn extends Component {
         const user = userCredential.user;
         document.cookie="ls=1";
         console.log(document.cookie);
-        this.props.history.push('/dashboard');
+        this.props.history.push('/Report');
         // ...
       })
       .catch((error) => {
